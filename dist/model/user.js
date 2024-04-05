80d8976b46a6ae8a562c039f75b286125eeed72f
natalia <nataliaguerrini1@gmail.com>
2024-04-05 14:41:15 +0200

    modificato gitignore

--- dist/model/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRole = void 0;
-exports.UserRole = {
-    student: 1,
-    professor: 2,
-};
-//# sourceMappingURL=user.js.map
